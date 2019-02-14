320ccb96242fdcae54dc7a868211d0f0692b9bb6
MarcosPanda <marcosleandronascimento@outlook.com>
2019-02-14 03:19:25 -0200

    Teste funcionando

--- src/test/java/JUnit5ExampleTest.java
+++ src/test/java/JUnit5ExampleTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class JUnit5ExampleTest {
@@ -5,5 +6,10 @@ class JUnit5ExampleTest {
     @Test
     void justAnExample() {
         System.out.println("This test method should be run");
+        System.out.println("");
+        System.out.println("First step");
+        Assertions.assertEquals(4, 4, "Thats fine - test ok");
+        System.out.println("Second step");
+        Assertions.assertEquals(4, 3,"3 is different of 4 - test failed");
     }
 }
